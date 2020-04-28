aacecb246783b6ccb8f3fc18200059079c8f7853
peretarrida13 <58071471+peretarrida13@users.noreply.github.com>
2020-04-28 21:44:49 +0200

    Add files via upload

---
+++ Codi/Cjt_Especies.cc
@@ -0,0 +1,41 @@
+#include "Cjt_Especies.hh"
+using namespace std;
+
+Cjt_Especies::Cjt_Especies(){}
+
+void Cjt_Especies::crea_especie(string& id, string& gen){
+
+}
+
+Especie Cjt_Especies::consultar_especie(string id) const{
+
+}
+
+double Cjt_Especies::distancia(const Especie& a, const Especie& b){
+
+}
+
+void Cjt_Especies::eliminar_especie(string& id){
+
+}
+
+
+bool Cjt_Especies::existe_especie(string& id) const{
+
+}
+
+void Cjt_Especies::lee_cjt_especies(){
+
+}
+
+bool Cjt_Especies::esta_buit(){
+
+}
+
+void Cjt_Especies::imprime_cjt_especies() const{
+
+}
+
+void Cjt_Especies::imprimir_tabla_distancias()const{
+
+}
