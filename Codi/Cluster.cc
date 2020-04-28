91c6414a912c31a3eaf3590637b527a91461e958
peretarrida13 <58071471+peretarrida13@users.noreply.github.com>
2020-04-28 21:51:06 +0200

    Add files via upload

---
+++ Codi/Cluster.cc
@@ -0,0 +1,16 @@
+#include "Cluster.hh"
+using namespace std;
+
+Cluster::Cluster(Especie& e){
+
+}
+
+Cluster::Cluster(Cluster esquerra, Cluster dret){
+
+}
+
+Cluster::~Cluster(){}
+
+void Cluster::imprimeix_cluster(){
+
+}
