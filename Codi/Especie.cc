fb2788966e8ff9a8076e6baf7c5f9c44a59941fe
peretarrida13 <58071471+peretarrida13@users.noreply.github.com>
2020-04-28 21:35:33 +0200

    Add files via upload

---
+++ Codi/Especie.cc
@@ -0,0 +1,16 @@
+#include "Especie.hh"
+using namespace std;
+
+Especie::Especie(){
+    gen = "0";
+}
+
+Especie::Especie(string& g, int& k){}
+
+Especie::~Especie(){}
+
+vector<string> Especie::obtener_kmer() const{}
+
+string Especie::obtener_gen() const{
+
+}
