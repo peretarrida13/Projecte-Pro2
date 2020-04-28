c79ecd2c8576a84d849360aa5fcd59fef41ed45b
peretarrida13 <58071471+peretarrida13@users.noreply.github.com>
2020-04-28 21:55:21 +0200

    Add files via upload

---
+++ Codi/Cjt_Cluster.cc
@@ -0,0 +1,9 @@
+#include "Cjt_Cluster.hh"
+
+void Cjt_Cluster::inicialitza_clustrers(Cjt_Especies& e){}
+
+void Cjt_Cluster::ejecuta_paso_wpgma(){}
+
+Cluster Cjt_Cluster::consultar_cluster(string id) const{}
+
+void Cjt_Cluster::imprime_arbol_filogenetio(){}
