0e52ec221fe4e01faa525e4b84df442cba7ddc6f
peretarrida13 <58071471+peretarrida13@users.noreply.github.com>
2020-04-30 16:43:04 +0200

    Add files via upload

--- Codi/Cluster.cc
+++ Codi/Cluster.cc
@@ -2,15 +2,21 @@
 using namespace std;
 
 Cluster::Cluster(Especie& e){
-
+    pair a; 
+    a.fist() = e.obtener_gen();
 }
 
 Cluster::Cluster(Cluster esquerra, Cluster dret){
-
+    cluster.left() = esquerra;
+    cluster.right() = dret;
 }
 
 Cluster::~Cluster(){}
 
 void Cluster::imprimeix_cluster(){
-
+    if(not a.empty()){
+        cout << a.value() << endl;
+        imprimeix_cluster(a.right());
+        imprimeix_cluster(a.left());
+    }
 }
